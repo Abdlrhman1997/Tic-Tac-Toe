5ade4ff595311749b4a64107ead1f3ecb221d115
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-03-20 17:45:17 -0700

    Edit players turn title paragraph when it's another turn

--- index.js
+++ index.js
@@ -11,16 +11,20 @@ let Player = (name) => {
 const displayGameBoard = (() => {
   let gameBoardDivs = document.querySelectorAll(".square");
 
+  let playerTitleParag = document.querySelector("#player");
+
   let playerTurn = "x";
   for (let i = 0; i < gameBoardDivs.length; i++) {
     gameBoardDivs[i].addEventListener("click", (e) => {
       if (playerTurn == "x" && e.target.textContent === "") {
         e.target.textContent = "x";
         playerTurn = "o";
+        playerTitleParag.textContent = "Player O's turn";
       } else {
         if (e.target.textContent == "") {
           e.target.textContent = "o";
           playerTurn = "x";
+          playerTitleParag.textContent = "Player X's turn";
         }
       }
     });
