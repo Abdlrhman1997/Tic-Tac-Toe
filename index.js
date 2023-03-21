92d8df6179d220e9d4964134fbd7f78704ef202a
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-03-20 20:59:41 -0700

    assign div square textContent to the gameboard object

--- index.js
+++ index.js
@@ -16,15 +16,21 @@ const displayGameBoard = (() => {
   let playerTurn = "x";
   for (let i = 0; i < gameBoardDivs.length; i++) {
     gameBoardDivs[i].addEventListener("click", (e) => {
+      let divId = e.target.id;
+
       if (playerTurn == "x" && e.target.textContent === "") {
         e.target.textContent = "x";
+        gameBoard.gameBoard[divId - 1] = e.target.textContent;
         playerTurn = "o";
         playerTitleParag.textContent = "Player O's turn";
+        console.log(gameBoard.gameBoard);
       } else {
         if (e.target.textContent == "") {
           e.target.textContent = "o";
+          gameBoard.gameBoard[divId - 1] = e.target.textContent;
           playerTurn = "x";
           playerTitleParag.textContent = "Player X's turn";
+          console.log(gameBoard.gameBoard);
         }
       }
     });
