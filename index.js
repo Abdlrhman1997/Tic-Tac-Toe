581f656b2991389edd0b3d0cf249821e97f72431
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-03-20 22:05:08 -0700

    Add two functions: 1.function checkWinner 2.checkXorO

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let gameBoard = {
-  // gameBoard: ["", "", "", "", "", "", "", "", ""],
   gameBoard: ["", "", "", "", "", "", "", "", ""],
 };
 
@@ -10,10 +9,37 @@ let Player = (name) => {
 
 const displayGameBoard = (() => {
   let gameBoardDivs = document.querySelectorAll(".square");
-
   let playerTitleParag = document.querySelector("#player");
-
   let playerTurn = "x";
+
+  function checkXorO(gameBoardValue) {
+    if (gameBoardValue === "x") {
+      playerTitleParag.textContent = `Player X win!`;
+    } else if (gameBoardValue === "o") {
+      playerTitleParag.textContent = `Player O win!`;
+    }
+  }
+
+  function checkWinner(gameBoard) {
+    if (gameBoard[0] === gameBoard[3] && gameBoard[0] === gameBoard[6]) {
+      return checkXorO(gameBoard[0]);
+    } else if (gameBoard[2] === gameBoard[5] && gameBoard[2] === gameBoard[8]) {
+      return checkXorO(gameBoard[2]);
+    } else if (gameBoard[0] === gameBoard[1] && gameBoard[0] === gameBoard[2]) {
+      return checkXorO(gameBoard[0]);
+    } else if (gameBoard[6] === gameBoard[7] && gameBoard[6] === gameBoard[8]) {
+      return checkXorO(gameBoard[6]);
+    } else if (gameBoard[0] === gameBoard[4] && gameBoard[0] === gameBoard[8]) {
+      return checkXorO(gameBoard[0]);
+    } else if (gameBoard[2] === gameBoard[4] && gameBoard[2] === gameBoard[6]) {
+      return checkXorO(gameBoard[2]);
+    } else if (gameBoard[3] === gameBoard[4] && gameBoard[3] === gameBoard[5]) {
+      return checkXorO(gameBoard[3]);
+    } else if (gameBoard[1] === gameBoard[4] && gameBoard[1] === gameBoard[7]) {
+      return checkXorO(gameBoard[1]);
+    }
+  }
+
   for (let i = 0; i < gameBoardDivs.length; i++) {
     gameBoardDivs[i].addEventListener("click", (e) => {
       let divId = e.target.id;
@@ -23,6 +49,7 @@ const displayGameBoard = (() => {
         gameBoard.gameBoard[divId - 1] = e.target.textContent;
         playerTurn = "o";
         playerTitleParag.textContent = "Player O's turn";
+        checkWinner(gameBoard.gameBoard);
         console.log(gameBoard.gameBoard);
       } else {
         if (e.target.textContent == "") {
@@ -30,6 +57,7 @@ const displayGameBoard = (() => {
           gameBoard.gameBoard[divId - 1] = e.target.textContent;
           playerTurn = "x";
           playerTitleParag.textContent = "Player X's turn";
+          checkWinner(gameBoard.gameBoard);
           console.log(gameBoard.gameBoard);
         }
       }
